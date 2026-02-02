de4c2dc95e8aecf329b4e4da524c3c9dc62270be
Andrii Shukalo <shukaload@gmail.com>
2026-02-02 14:43:35 +0200

    Implement cache-decorator

--- app/main.py
+++ app/main.py
@@ -4,13 +4,15 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cache_results = {}
 
-    def inner(*args) -> Any:
-        if args in cache_results:
-            print("Getting from cache")
-            return cache_results[args]
+    def inner(*args, **kwargs) -> Any:
+        argumets = (args, tuple(kwargs.values()))
 
-        result = func(*args)
-        cache_results[args] = result
+        if argumets in cache_results:
+            print("Getting from cache")
+            return cache_results[argumets]
+
+        result = func(*args, **kwargs)
+        cache_results[argumets] = result
         print("Calculating new result")
         return result
     return inner

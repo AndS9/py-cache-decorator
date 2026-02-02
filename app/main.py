b1fcf6a5f16eaff707988533443c5859670aba93
Andrii Shukalo <shukaload@gmail.com>
2026-02-02 02:06:14 +0200

    Implement cache-decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-from typing import Callable
+from typing import Callable, Any
+# from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    # @wraps(func)
+    cache_results = {}
+
+    def inner(*args, **kwargs) -> Any:
+        if kwargs:
+            print("This function can't operate with keyword arguments")
+            return None
+
+        if args in cache_results.keys():
+            print("Getting from cache")
+            return cache_results[args]
+
+        result = func(*args, **kwargs)
+        cache_results[args] = result
+        print("Calculating new result")
+        return result
+    return inner
